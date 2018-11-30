b609292b59ac26335f11c2c6835fc261f63d1668
andreasholteritter <ritand16@student.westerdals.no>
2018-11-30 18:08:35 +0100

    Update FoodApplication.java

--- src/main/java/com/example/herokupipeexample/FoodApplication.java
+++ src/main/java/com/example/herokupipeexample/FoodApplication.java
@@ -2,11 +2,17 @@ package com.example.herokupipeexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class FoodApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(LogzioLogbackExample.class);
+		
+		logger.info("Starting SpringApplication");
 		SpringApplication.run(FoodApplication.class, args);
+		logger.info("SpringApplication Started");
 	}
 }
